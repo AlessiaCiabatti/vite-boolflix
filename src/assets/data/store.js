9aa202e58466d6f5e6e70538debe7fc46c74ecc9
AlessiaCiabatti <alessiaciaba@gmail.com>
2024-04-04 15:28:56 +0200

    [add] Header-form-div

--- src/assets/data/store.js
+++ src/assets/data/store.js
@@ -3,7 +3,7 @@ import { reactive } from "vue"
 export const store = reactive({
   apiUrl: 'https://api.themoviedb.org/3/search/',
   queryParams:{
-    query: 'fantozzi',
+    query: '',
     language: 'it-IT',
     api_key: '4ff17c9119ea91b125eff2d9e4974685',
   },
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -18,13 +18,13 @@ import { store } from '../assets/data/store';
 <nav class="navbar bg-body-tertiary">
   <div class="container-fluid">
     <a class="navbar-brand">BOOLFLIX</a>
-    <form class="d-flex" role="search">
+    <div class="d-flex" role="search">
       <input
       v-model.trim="this.store.queryParams.query"
       class="form-control me-2" type="text" placeholder="Cerca un film">
 
       <button @click="$emit('startSearch')" class="btn btn-primary">Search</button>
-    </form>
+    </div>
   </div>
 </nav>
 
